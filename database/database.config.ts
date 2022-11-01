ec6c20d26df23c658f9d44d4ad86656d828c6c76
Pham.T.Hung <phamthanh.hung.work@gmail.com>
2022-11-01 15:23:06 +0700

    update

--- database/database.config.ts
+++ database/database.config.ts
@@ -9,7 +9,7 @@ import { User } from 'src/module/core/users/entities/user.entity';
 export class DatabaseConfig {
   config: TypeOrmModuleOptions = {
     type: 'postgres',
-    url: 'postgres://basjmbtjfnqzmk:9078a1e76ac5e69a278584dd5f903277d820438c133f4688ac3933b6f4b3917e@ec2-23-20-140-229.compute-1.amazonaws.com:5432/d7l5d0cc8qpv8m',
+    url: process.env.DATABASE_URL,
     entities: [Manager, User, Dish, DishMeal, Meal, Menu],
     extra: {
       ssl: {
